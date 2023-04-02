16421dd0043b95edb1a75f7c41623bc21e0040a4
jojco <hi@jojco.me>
2023-04-02 18:01:53 +0000

    update scd30

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>Index</h1>
-    <h2>co2Concentration {{ scd30.co2Concentration }}</h2>
-    <h2>temperature {{ scd30.temperature }}</h2>
+    <h2>co2Concentration: {{ scd30.co2Concentration }}</h2>
+    <h2>temperature: {{ scd30.temperature }}</h2>
+    <h2>relativeHumidity: {{ scd30.relativeHumidity }}</h2>
     <h2>Hello {{ hello }}</h2>
   </div>
 </template>
--- server/api/scd30.ts
+++ server/api/scd30.ts
@@ -1,23 +1,19 @@
 import { SCD30 } from 'scd30-node';
 
-interface SCD30Mesurement {
-  co2Concentration: number;
-  temperature: number;
-  relativeHumidity: number;
-}
-
-let measurement: SCD30Mesurement;
-
 export default defineEventHandler(async () => {
   const scd30 = await SCD30.connect();
   await scd30.startContinuousMeasurement();
-  measurement = await scd30.readMeasurement();
-  // console.log(`CO2 Concentration: ${measurement.co2Concentration} ppm`);
+  const measurement = await scd30.readMeasurement();
+
+  console.log(`CO2 Concentration: ${measurement.co2Concentration} ppm`);
   console.log(`Temperature: ${measurement.temperature} °C`);
-  // console.log(`Humidity: ${measurement.relativeHumidity} %`);
+  console.log(`Humidity: ${measurement.relativeHumidity} %`);
+
+  await scd30.stopContinuousMeasurement();
 
   return {
     co2Concentration: measurement.co2Concentration,
-    temperature: measurement.temperature
+    temperature: measurement.temperature,
+    relativeHumidity: measurement.relativeHumidity
   };
 });
