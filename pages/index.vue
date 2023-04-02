b04d49e8b0b81cf9abb9a3c87b56c2cf977bb533
jojco <hi@jojco.me>
2023-04-02 16:47:49 +0000

    server api update

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,16 @@
   <div>
     <h1>Index</h1>
     <h2>SCD30 {{ scd30 }}</h2>
+    <h2>Hello {{ hello }}</h2>
   </div>
 </template>
 
 <script setup>
 const { data: scd30 } = await useFetch('/scd30');
-console.log(scd30);
+const { data: hello } = await useFetch('/api/hello');
+
+console.log('scd30', scd30);
+console.log('hello', hello);
 </script>
 
 <style lang="scss" scoped></style>
